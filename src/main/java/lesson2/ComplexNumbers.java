83b41e41db0445dc54c0f10980c344a8a6fcb159
Mariya Korotkevich <ma.korotkevich@gmail.com>
2023-03-11 22:28:24 +0300

    complex numbers

--- src/main/java/lesson2/ComplexNumbers.java
+++ src/main/java/lesson2/ComplexNumbers.java
@@ -1,4 +1,43 @@
 package lesson2;
 
 public class ComplexNumbers {
+    private double a;
+    private double b;
+
+    public ComplexNumbers(double a) {
+        this.a = a;
+    }
+
+    public ComplexNumbers(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public ComplexNumbers sum (ComplexNumbers number){
+        return null;
+    }
+
+    public ComplexNumbers minus (ComplexNumbers number){
+        return null;
+    }
+
+    public ComplexNumbers umnoj(ComplexNumbers number){
+        return null;
+    }
+    public ComplexNumbers abs(){
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        String result;
+        if (b == 0){
+            result = "" + a;
+        } else if (a == 0){
+            result = b + "i";
+        } else {
+            result = a + " + " + b + "i";
+        }
+        return result;
+    }
 }

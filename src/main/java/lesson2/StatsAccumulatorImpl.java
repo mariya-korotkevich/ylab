6e64d8c2f1e9a92a840d6110236e7ca4eb0697f4
Mariya Korotkevich <ma.korotkevich@gmail.com>
2023-03-12 17:42:56 +0300

    StatsAccumulatorImpl

--- src/main/java/lesson2/StatsAccumulatorImpl.java
+++ src/main/java/lesson2/StatsAccumulatorImpl.java
@@ -1,21 +1,22 @@
 package lesson2;
 
-public class StatsAccumulatorImpl implements StatsAccumulator{
+public class StatsAccumulatorImpl implements StatsAccumulator {
     private int min = Integer.MAX_VALUE;
     private int max = Integer.MIN_VALUE;
     private int count = 0;
-    private int sum = 0;
+
+    private double avg = 0;
 
     @Override
     public void add(int value) {
-        if (value < min){
+        if (value < min) {
             min = value;
         }
-        if (value > max){
+        if (value > max) {
             max = value;
         }
         count++;
-        sum += value;
+        avg = ((avg * (count - 1)) + value) / count;
     }
 
     @Override
@@ -35,6 +36,6 @@ public class StatsAccumulatorImpl implements StatsAccumulator{
 
     @Override
     public Double getAvg() {
-        return (double) sum / count;
+        return avg;
     }
 }

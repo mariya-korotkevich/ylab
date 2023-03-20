851a04974f961f2218b3c0b434ff0dcd52148946
Mariya Korotkevich <ma.korotkevich@gmail.com>
2023-03-20 14:30:22 +0300

    file sorter

--- src/main/java/lesson3/fileSort/SorterTest.java
+++ src/main/java/lesson3/fileSort/SorterTest.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 
 public class SorterTest {
     public static void main(String[] args) throws IOException {
-        File dataFile = new Generator().generate("data.txt", 500_000);
+        File dataFile = new Generator().generate("data.txt", 5_000_000);
         System.out.println(new Validator(dataFile).isSorted()); // false
         File sortedFile = new Sorter().sortFile(dataFile);
         System.out.println(new Validator(sortedFile).isSorted()); // true

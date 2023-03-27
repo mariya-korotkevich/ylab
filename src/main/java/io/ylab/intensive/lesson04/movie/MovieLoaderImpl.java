58157f39596921accf838901deaa066a7467fd31
Mariya Korotkevich <ma.korotkevich@gmail.com>
2023-03-27 11:29:32 +0300

    movie loader

--- src/main/java/io/ylab/intensive/lesson04/movie/MovieLoaderImpl.java
+++ src/main/java/io/ylab/intensive/lesson04/movie/MovieLoaderImpl.java
@@ -25,56 +25,6 @@ public class MovieLoaderImpl implements MovieLoader {
         writeListToDB(movies);
     }
 
-    private void writeListToDB(List<Movie> movies) {
-
-        String query = "insert into movie\n"
-                + "\t(year, length, title, subject, actors, actress, director, popularity, awards)\n"
-                + "\tvalues (?, ?, ?, ?, ?, ?, ?, ?, ?)\n"
-                + ";";
-
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(query)) {
-            for (Movie movie : movies) {
-                setInt(statement, 1, movie.getYear());
-                setInt(statement, 2, movie.getLength());
-                setString(statement, 3, movie.getTitle());
-                setString(statement, 4, movie.getSubject());
-                setString(statement, 5, movie.getActors());
-                setString(statement, 6, movie.getActress());
-                setString(statement, 7, movie.getDirector());
-                setInt(statement, 8, movie.getPopularity());
-                setBoolean(statement, 9, movie.getAwards());
-                statement.execute();
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void setInt(PreparedStatement statement, int index, Integer value) throws SQLException {
-        if (value == null) {
-            statement.setNull(index, Types.INTEGER);
-        } else {
-            statement.setInt(index, value);
-        }
-    }
-
-    private void setString(PreparedStatement statement, int index, String value) throws SQLException {
-        if (value == null) {
-            statement.setNull(index, Types.VARCHAR);
-        } else {
-            statement.setString(index, value);
-        }
-    }
-
-    private void setBoolean(PreparedStatement statement, int index, Boolean value) throws SQLException {
-        if (value == null) {
-            statement.setNull(index, Types.BOOLEAN);
-        } else {
-            statement.setBoolean(index, value);
-        }
-    }
-
     private List<Movie> readCsvFile(File csvFile) {
         List<Movie> movies = new ArrayList<>();
         try (Scanner scanner = new Scanner(new FileInputStream(csvFile))) {
@@ -104,5 +54,52 @@ public class MovieLoaderImpl implements MovieLoader {
         movie.setPopularity(data[7].isEmpty() ? null : Integer.valueOf(data[7]));
         movie.setAwards(data[8].isEmpty() ? null : Boolean.valueOf(data[8]));
         return movie;
+    }
+    private void writeListToDB(List<Movie> movies) {
+        String query = "insert into movie\n"
+                + "(year, length, title, subject, actors, actress, director, popularity, awards)\n"
+                + "values (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+            for (Movie movie : movies) {
+                setInt(statement, 1, movie.getYear());
+                setInt(statement, 2, movie.getLength());
+                setString(statement, 3, movie.getTitle());
+                setString(statement, 4, movie.getSubject());
+                setString(statement, 5, movie.getActors());
+                setString(statement, 6, movie.getActress());
+                setString(statement, 7, movie.getDirector());
+                setInt(statement, 8, movie.getPopularity());
+                setBoolean(statement, 9, movie.getAwards());
+                statement.addBatch();
+            }
+            statement.executeBatch();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void setInt(PreparedStatement statement, int index, Integer value) throws SQLException {
+        if (value == null) {
+            statement.setNull(index, Types.INTEGER);
+        } else {
+            statement.setInt(index, value);
+        }
+    }
+
+    private void setString(PreparedStatement statement, int index, String value) throws SQLException {
+        if (value == null) {
+            statement.setNull(index, Types.VARCHAR);
+        } else {
+            statement.setString(index, value);
+        }
+    }
+
+    private void setBoolean(PreparedStatement statement, int index, Boolean value) throws SQLException {
+        if (value == null) {
+            statement.setNull(index, Types.BOOLEAN);
+        } else {
+            statement.setBoolean(index, value);
+        }
     }
 }
